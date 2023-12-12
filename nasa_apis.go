cc0036df1a7468c2a7c4a52c6ee64c3e33ed6ca0
Vanya Mishra <vanyamishra@192.168.0.106>
2023-12-13 00:08:20 +0530

    BLINE: Adding test file

--- nasa_apis.go
+++ nasa_apis.go
@@ -21,91 +21,98 @@ func main() {
 	//Adding mapping for Astronomy Picture of the Day API
 	const mappingApod = "/apod"
 	const apiUrlApod = baseUrl + "/planetary/apod"
-	createExternalGetAPIMapping(client, router, apiUrlApod, mappingApod)
+	CreateExternalGetAPIMapping(client, router, apiUrlApod, mappingApod)
 
 	//Adding mapping for Mars Rover Photos API
 	const mappingMarsRoverPhotos = "/mars-rover-photos/:earth_date"
 	const apiUrlMarsRoverPhotos = baseUrl + "/mars-photos/api/v1/rovers/curiosity/photos"
 	const paramName = "earth_date"
-	const promptMessage = "Please enter the earth date in YYYY-MM-DD format to retrieve the Mars Rover Photos."
-	createExternalGetAPIMappingWithParameter(client, router, apiUrlMarsRoverPhotos, mappingMarsRoverPhotos, promptMessage, paramName)
+	CreateExternalGetAPIMappingWithParameter(client, router, apiUrlMarsRoverPhotos, mappingMarsRoverPhotos, paramName)
 
 	router.Run(":8080")
 }
 
-func createExternalGetAPIMapping(client *http.Client, router *gin.Engine, url string, mapping string) {
+func CreateExternalGetAPIMapping(client *http.Client, router *gin.Engine, url string, mapping string) {
 	router.GET(mapping, func(c *gin.Context) {
-		//Create the GET request
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s?api_key=%s", url, apiKey), nil)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the API request"})
-			return
-		}
-
-		//Send the request
-		resp, err := client.Do(req)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send the API request"})
-			return
-		}
-		defer resp.Body.Close()
-
-		//Display the response
-		if resp.StatusCode == http.StatusOK {
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read the response body"})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{"result": string(body)})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error: %d - %s", resp.StatusCode, resp.Status)})
-		}
+		HandleAPICall(c, client, router, url, mapping)
 	})
 }
 
-func createExternalGetAPIMappingWithParameter(client *http.Client, router *gin.Engine, url string, mapping string, promptMessage string, paramName string) {
+func CreateExternalGetAPIMappingWithParameter(client *http.Client, router *gin.Engine, url string, mapping string, paramName string) {
 	router.GET(mapping, func(c *gin.Context) {
-		//Retrieve a single parameter
-		paramValue := c.Param(paramName)
-
-		//Validate the parameter
-		error := validateParam(paramName, paramValue)
-		if error != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
-			return
-		}
-
-		//Create the GET request
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s?%s=%s&api_key=%s", url, paramName, paramValue, apiKey), nil)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the API request"})
-			return
-		}
-
-		//Send the request
-		resp, err := client.Do(req)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send the API request"})
-			return
-		}
-		defer resp.Body.Close()
-
-		//Display the response
-		if resp.StatusCode == http.StatusOK {
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read the response body"})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{"result": string(body)})
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error: %d - %s", resp.StatusCode, resp.Status)})
-		}
+		HandleAPICallWithParameter(c, client, router, url, mapping, paramName)
 	})
 }
 
-func validateParam(paramName string, paramValue string) error {
+func HandleAPICall(c *gin.Context, client *http.Client, router *gin.Engine, url string, mapping string) {
+	requestURL := fmt.Sprintf("%s?api_key=%s", url, apiKey)
+	req, err := CreateRequest(requestURL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the API request"})
+		return
+	}
+
+	resp, err := SendRequest(client, req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send the API request"})
+		return
+	}
+	defer resp.Body.Close()
+
+	HandleAPIResponse(c, resp)
+}
+
+func HandleAPICallWithParameter(c *gin.Context, client *http.Client, router *gin.Engine, url string, mapping string, paramName string) {
+	paramValue := c.Param(paramName)
+	error := ValidateParam(paramName, paramValue)
+	if error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
+		return
+	}
+
+	requestURL := fmt.Sprintf("%s?%s=%s&api_key=%s", url, paramName, paramValue, apiKey)
+	req, err := CreateRequest(requestURL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the API request"})
+		return
+	}
+
+	resp, err := SendRequest(client, req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send the API request"})
+		return
+	}
+	defer resp.Body.Close()
+
+	HandleAPIResponse(c, resp)
+}
+
+func CreateRequest(requestURL string) (*http.Request, error) {
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func SendRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	return client.Do(req)
+}
+
+func HandleAPIResponse(c *gin.Context, resp *http.Response) {
+	if resp.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read the response body"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"result": string(body)})
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error: %d - %s", resp.StatusCode, resp.Status)})
+	}
+}
+
+func ValidateParam(paramName string, paramValue string) error {
 	if paramName == "earth_date" {
 		// Define a regular expression pattern for "YYYY-MM-DD"
 		datePattern := "^\\d{4}-\\d{2}-\\d{2}$"
