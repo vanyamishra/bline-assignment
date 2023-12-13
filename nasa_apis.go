e59358efba74b74e34e22087038f587531cad71e
Vanya Mishra <vanyamishra@192.168.0.104>
2023-12-13 22:13:29 +0530

    BLINE: Further clean up, adding unit tests

--- nasa_apis.go
+++ nasa_apis.go
@@ -21,68 +21,81 @@ func main() {
 	//Adding mapping for Astronomy Picture of the Day API
 	router.GET("nasa/apod", func(c *gin.Context) {
 		requestURL := fmt.Sprintf("%s?api_key=%s", baseUrl+"/planetary/apod", apiKey)
-		ManageExternalAPIRequest(c, client, requestURL)
+		body, status, err := manageExternalAPIRequest(client, requestURL)
+		if err != nil {
+			c.JSON(status, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"result": body})
+		}
 	})
 
 	//Adding mapping for Mars Rover Photos API
 	router.GET("nasa/mars-rover-photos/:earth_date", func(c *gin.Context) {
 		paramName := "earth_date"
 		paramValue := c.Param(paramName)
-		error := ValidateParam(paramName, paramValue)
+		error := validateParam(paramName, paramValue)
 		if error != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
 			return
 		}
 		requestURL := fmt.Sprintf("%s?%s=%s&api_key=%s", baseUrl+"/mars-photos/api/v1/rovers/curiosity/photos", paramName, paramValue, apiKey)
-		ManageExternalAPIRequest(c, client, requestURL)
+		body, status, err := manageExternalAPIRequest(client, requestURL)
+		if err != nil {
+			c.JSON(status, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"result": body})
+		}
 	})
 
 	router.Run(":8080")
 }
 
-func ManageExternalAPIRequest(c *gin.Context, client *http.Client, requestURL string) {
-	req, err := CreateExternalRequest(requestURL)
+func manageExternalAPIRequest(client *http.Client, requestURL string) (string, int, error) {
+	req, err := createRequest(requestURL)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create the API request"})
-		return
+		return "", http.StatusInternalServerError, err
 	}
 
-	resp, err := SendExternalAPIRequest(client, req)
+	//Send request
+	resp, err := sendRequest(client, req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send the API request"})
-		return
+		return "", http.StatusInternalServerError, err
 	}
 	defer resp.Body.Close()
 
-	HandleExternalAPIResponse(c, resp)
+	body, status, error := handleAPIResponse(resp)
+	return body, status, error
 }
 
-func CreateExternalRequest(requestURL string) (*http.Request, error) {
+func sendRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.New("Failed to send the API request")
+	}
+	return resp, nil
+}
+
+func createRequest(requestURL string) (*http.Request, error) {
 	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Failed to create the API request")
 	}
 	return req, nil
 }
 
-func SendExternalAPIRequest(client *http.Client, req *http.Request) (*http.Response, error) {
-	return client.Do(req)
-}
-
-func HandleExternalAPIResponse(c *gin.Context, resp *http.Response) {
+func handleAPIResponse(resp *http.Response) (string, int, error) {
 	if resp.StatusCode == http.StatusOK {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read the response body"})
-			return
+			return "", http.StatusInternalServerError, errors.New("Failed to read the response body")
 		}
-		c.JSON(http.StatusOK, gin.H{"result": string(body)})
+		return string(body), http.StatusOK, nil
 	} else {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error: %d - %s", resp.StatusCode, resp.Status)})
+		return "", http.StatusInternalServerError, errors.New(fmt.Sprintf("Error: %d - %s", resp.StatusCode, resp.Status))
 	}
 }
 
-func ValidateParam(paramName string, paramValue string) error {
+func validateParam(paramName string, paramValue string) error {
 	if paramName == "earth_date" {
 		// Define a regular expression pattern for "YYYY-MM-DD"
 		datePattern := "^\\d{4}-\\d{2}-\\d{2}$"
--- nasa_apis_test.go
+++ nasa_apis_test.go
@@ -1,1 +1,131 @@
 package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateExternalRequest(t *testing.T) {
+	apiURL := "http://localhost:8080/sample"
+	req, err := createRequest(apiURL)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+	assert.Equal(t, apiURL, req.URL.String())
+}
+
+func TestManageExternalAPIRequestStatusCodeOK(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//Send a mock status code
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"mock_key": "mock_value"}`))
+	}))
+	defer server.Close()
+
+	// gin.SetMode(gin.TestMode)
+	// w := httptest.NewRecorder()
+	// ctx, _ := gin.CreateTestContext(w)
+	// ctx.Request = &http.Request{
+	// 	Header: make(http.Header),
+	// 	URL:    &url.URL{Path: "http://localhost:8080/sample"},
+	// }
+	client := &http.Client{} //TODO: This should be mocked.
+	apiURL := "http://localhost:8080/sample"
+	body, status, err := manageExternalAPIRequest(client, apiURL)
+	assert.NotNil(t, err)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Failed to send the API request")
+	assert.Empty(t, body)
+	assert.Equal(t, status, 500)
+}
+
+func TestHandleExternalAPIResponseError(t *testing.T) {
+	// gin.SetMode(gin.TestMode)
+	// w := httptest.NewRecorder()
+	// ctx, _ := gin.CreateTestContext(w)
+	// ctx.Request = &http.Request{
+	// 	Header: make(http.Header),
+	// 	URL:    &url.URL{},
+	// }
+	resp := &http.Response{}
+	resp.Body = http.NoBody
+	resp.Status = http.StatusText(http.StatusBadGateway)
+	resp.StatusCode = http.StatusBadGateway
+	body, status, err := handleAPIResponse(resp)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Bad Gateway")
+	assert.Empty(t, body)
+	assert.Equal(t, status, 500)
+}
+
+func TestHandleExternalAPIResponseNoError(t *testing.T) {
+	// gin.SetMode(gin.TestMode)
+	// w := httptest.NewRecorder()
+	// ctx, _ := gin.CreateTestContext(w)
+	// ctx.Request = &http.Request{
+	// 	Header: make(http.Header),
+	// 	URL:    &url.URL{},
+	// }
+	resp := &http.Response{Body: io.NopCloser(bytes.NewBufferString("Hello World"))}
+	resp.Status = http.StatusText(http.StatusOK)
+	resp.StatusCode = http.StatusOK
+	body, status, err := handleAPIResponse(resp)
+	assert.NoError(t, err)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, body)
+	assert.Contains(t, body, "Hello World")
+	assert.Equal(t, status, 200)
+}
+
+func TestValidateParameterValidResult(t *testing.T) {
+	paramName := "earth_date"
+	paramValue := "2022-09-08"
+	err := validateParam(paramName, paramValue)
+	assert.NoError(t, err)
+}
+
+func TestValidateParameterInvalidResult(t *testing.T) {
+	paramName := "earth_date"
+	paramValue := "2022-09"
+	err := validateParam(paramName, paramValue)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "Incorrect earth_date format.")
+}
+
+func TestValidateParameterNonExistentParameter(t *testing.T) {
+	paramName := "something_else"
+	paramValue := "2022-09"
+	err := validateParam(paramName, paramValue)
+	assert.NoError(t, err)
+	assert.Nil(t, err)
+}
+
+// func TestMain(t *testing.T) {
+// 	router := gin.Default()
+
+// 	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		// Simulate a successful API response with mock data
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(`{"mock_key": "mock_value"}`))
+// 	}))
+// 	defer mockServer.Close()
+
+// 	apiURL := mockServer.URL + "/nasa/apod"
+// 	//baseUrl = mockServer.URL
+
+// 	req, err := http.NewRequest("GET", apiURL, nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	assert.Equal(t, http.StatusOK, w.Code)
+
+// 	// expectedResponse := `{"data":"{\"mock_key\": \"mock_value\"}"}`
+// 	// assert.Equal(t, expectedResponse, w.Body.String())
+
+// }
